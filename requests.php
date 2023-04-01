5dfd584c9b3127aa5b15511559ab62f814f9b8a8
Diogo Alves <45717126+diogoasp@users.noreply.github.com>
2023-04-01 17:42:33 -0300

    Update requests.php

--- requests.php
+++ requests.php
@@ -1,21 +1,19 @@
 <?php
 
-// header("Content-Type: application/json");
-//   header("Acess-Control-Allow-Origin: *");
   header("Acess-Control-Allow-Methods: POST");
   header("Acess-Control-Allow-Headers: Acess-Control-Allow-Headers,Content-Type,Acess-Control-Allow-Methods, Authorization");
   header("Content-Type: application/json; charset=UTF-8");
 
 
-include 'dbconfig.php'; // include database connection file
+require 'dbconfig.php';
 
-// $data = json_decode(file_get_contents("php://input"), true); // collect input parameters and convert into readable format
-	
-// $fileName  =  $_FILES['sendimage']['name'];
-$temp  = explode(".",$_FILES['sendimage']['name']);
-$tempPath  =  $_FILES['sendimage']['tmp_name'];
-$fileSize  =  $_FILES['sendimage']['size'];
-$final_name = round(microtime(true)) . '.' . end($temp);
+$data = json_decode(file_get_contents("php://input"), true);
+if(isset($_FILES['sendimage'])){
+    $temp  = explode(".",$_FILES['sendimage']['name']);
+    $tempPath  =  $_FILES['sendimage']['tmp_name'];
+    $fileSize  =  $_FILES['sendimage']['size'];
+    $final_name = $_POST['fileName'];
+}
 		
 if(empty($tempPath))
 {
@@ -24,8 +22,7 @@ if(empty($tempPath))
 }
 else
 {
-	$upload_path = 'uploads/'; // set upload folder path 
-	// valid image extensions
+	$upload_path = 'uploads/';
 	$valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); 
 					
 	// allow valid image file formats
@@ -36,40 +33,35 @@ else
 		{
 			// check file size '5MB'
 			if($fileSize < 5000000){
-				move_uploaded_file($tempPath, $upload_path . $final_name); // move file from system temporary path to our upload folder path 
+				move_uploaded_file($tempPath, $upload_path . $final_name);
 			}
 			else{		
 				$errorMSG = json_encode(array("message" => "Sorry, your file is too large, please upload 5 MB size", "status" => false));	
-				// return $errorMSG;
 			}
 		}
 		else
 		{		
 			$errorMSG = json_encode(array("message" => "Sorry, file already exists check upload folder", "status" => false));	
-			// return $errorMSG;xxxxx
 		}
 	}
 	else
 	{		
 		$errorMSG = json_encode(array("message" => "Sorry, only JPG, JPEG, PNG & GIF files are allowed", "status" => false));	
-		// return $errorMSG;		
 	}
 }
 		
-// if no error caused, continue ....
 if(!isset($errorMSG))
 {
-	$query = mysqli_query($conn,'INSERT into tbl_image (name) VALUES("'.$final_name.'")');
+	$query = mysqli_query($conn,'INSERT into tbl_image (nome) VALUES("'.$final_name.'")');
 
 	$msg = json_encode(array("message" => "Image Uploaded Successfully", "url" => $final_name, "status" => 200));	
-  
-  echo $msg;
-  return $msg;
-  header("HTTP/1.1 200: $msg");
+    header("HTTP/1.1 200 $msg");
+    echo $msg;
+    exit();
   
 }else{
-  echo $errorMSG;
   header("HTTP/1.1 500 $errorMSG");
+  exit();
 }
 
 ?>
